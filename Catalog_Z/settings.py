4473a3a3952c85dbc7aedc033398d681f0f6f122
Saftarli <tofiqsefterli@gmail.com>
2024-03-28 19:00:06 +0400

    feat: create env.txt

--- Catalog_Z/settings.py
+++ Catalog_Z/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-
+DEBUG = True
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=list)
 CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS', cast=list)
 
